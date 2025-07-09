d8ff1ca1b1b8fe8c35def9d418ffdccc4636efbd
mengzijie.ymlf <mengzijie.ymlf@bytedance.com>
2025-07-10 01:25:08 +0800

    TRM

--- TRM/train.py
+++ TRM/train.py
@@ -1,8 +1,6 @@
 import time
-
 import torch
 import torch.nn as nn
-
 from dldemos.Transformer.data_load import (get_batch_indices, load_cn_vocab,
                                            load_en_vocab, load_train_data,
                                            maxlen)
